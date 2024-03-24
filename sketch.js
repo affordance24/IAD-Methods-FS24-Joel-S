10e52418e8fd924a7c786eed886e12a122aec737
affordance24 <joel.staub@zhdk.ch>
2024-03-24 22:00:09 +0100

    Upload 24.03

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400).parent('canvasContainer');
+  noLoop();
 }
 
 function draw() {
-  background(220);
+  background(255);
+  drawPattern();
+}
+
+function drawPattern() {
+  let tileSize = 20;
+  for (let x = 0; x < width; x += tileSize) {
+    for (let y = 0; y < height; y += tileSize) {
+      let r = random(255);
+      let g = random(255);
+      let b = random(255);
+      fill(r, g, b);
+      rect(x, y, tileSize, tileSize);
+    }
+  }
 }
